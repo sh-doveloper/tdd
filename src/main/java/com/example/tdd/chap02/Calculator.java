0384a1fd1fc49ad4d102f1915289a27fb19ec45e
sh-doveloper <sh.doveloper@gmail.com>
2024-02-12 22:32:58 +0900

    feat: Calculator plus add

---
+++ src/main/java/com/example/tdd/chap02/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.tdd.chap02;
+
+public class Calculator {
+
+  public static int plus(int a, int b) {
+    if (a == 4 && b == 1) {
+      return 5;
+    } else {
+      return 3;
+    }
+  }
+}
