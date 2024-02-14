f77d89ca1159b34a79e48ad39122eb3adb867632
sh-doveloper <sh.doveloper@gmail.com>
2024-02-14 21:29:21 +0900

    feat: 패스워드검증 테스트코드 3번 테스트 케이스 완료

--- src/main/java/com/example/tdd/chap02/PasswordStrengthMeter.java
+++ src/main/java/com/example/tdd/chap02/PasswordStrengthMeter.java
@@ -2,11 +2,25 @@ package com.example.tdd.chap02;
 
 public class PasswordStrengthMeter {
 
-  public PasswordStrength meter(String s) {
-    if (s.length() < 8) {
-      return PasswordStrength.NORMAL;
-    } else {
-      return PasswordStrength.STRONG;
+    public PasswordStrength meter(String s) {
+        if (s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+        boolean containsNum = meetContainingNumberCriteria(s);
+
+        if (!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
+        return PasswordStrength.STRONG;
+
     }
-  }
+
+    private boolean meetContainingNumberCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/example/tdd/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/com/example/tdd/chap02/PasswordStrengthMeterTest.java
@@ -6,22 +6,29 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
 
-  @Test
-  void meetsAllCriteria_Then_Strong() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("ab12!@AB");
-    assertEquals(PasswordStrength.STRONG, result);
-    PasswordStrength result2 = meter.meter("abc1!Add");
-    assertEquals(PasswordStrength.STRONG, result2);
-  }
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
 
-  @Test
-  void meetsOtherCriteria_except_for_Length_Then_Normal() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("ab12!@A");
-    assertEquals(PasswordStrength.NORMAL, result);
-    PasswordStrength result2 = meter.meter("Ab12!@c");
-    assertEquals(PasswordStrength.NORMAL, result2);
-  }
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result);
+        PasswordStrength result2 = meter.meter("Ab12!@c");
+        assertEquals(PasswordStrength.NORMAL, result2);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
 
 }
