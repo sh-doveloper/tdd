1e4bb8a7f4935e48b4dbf34da330414245adc3dd
sh-doveloper <sh.doveloper@gmail.com>
2024-02-16 16:22:49 +0900

    chore: github 테스트

--- src/test/java/com/example/tdd/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tdd/chap03/ExpiryDateCalculatorTest.java
@@ -26,4 +26,12 @@ public class ExpiryDateCalculatorTest {
         assetExpiryDate(LocalDate.of(2020, 1, 31), 10000, LocalDate.of(2020, 2, 29));
     }
 
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음() {
+        assetExpiryDate(LocalDate.of(2019, 1, 31), 10000, LocalDate.of(2019, 2, 28));
+        assetExpiryDate(LocalDate.of(2019, 5, 31), 10000, LocalDate.of(2019, 6, 30));
+        assetExpiryDate(LocalDate.of(2020, 1, 31), 10000, LocalDate.of(2020, 2, 29));
+    }
+
+
 }
